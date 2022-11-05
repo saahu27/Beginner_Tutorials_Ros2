0a4846ffe22fbc7857bb6793fb8767c2a34ad8cf
saahu27 <sahrudaypatti@gmail.com>
2022-11-04 23:20:02 -0400

    Progress Update, custom msgs

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/src/cpp_pubsub
+# Build directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/cpp_srvcli/src/add_two_ints_server.cpp
+++ src/cpp_srvcli/src/add_two_ints_server.cpp
@@ -7,33 +7,32 @@
  *
  */
 
-#include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
-
 #include <memory>
 
-void add(const std::shared_ptr
-          <example_interfaces::srv::AddTwoInts::Request> request,
-          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>
-          response) {
-  response->sum = request->a + request->b;
+#include "tutorial_interfaces/srv/add_three_ints.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+void add(
+  const std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Request> request,
+  std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Response> response)
+{
+  response->sum = request->a + request->b + request->c;
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
-  "Incoming request\na: %ld" " b: %ld",
-                request->a, request->b);
+  "Incoming request\na: %ld" " b: %ld" " c: %ld",
+  request->a, request->b, request->c);
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
-  "sending back response: [%ld]", (int)response->sum);
+  "sending back response: [%ld]", (long int)response->sum);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
-  std::shared_ptr<rclcpp::Node> node =
-  rclcpp::Node::make_shared("add_two_ints_server");
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_three_ints_server");
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>
-    ("add_two_ints", &add);
+  rclcpp::Service<tutorial_interfaces::srv::AddThreeInts>::SharedPtr service =
+    node->create_service<tutorial_interfaces::srv::AddThreeInts>("add_three_ints", &add);
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add three ints.");
 
   rclcpp::spin(node);
   rclcpp::shutdown();
