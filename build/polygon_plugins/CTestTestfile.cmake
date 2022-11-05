909897b3fdabb1888ec125ab17d55b3922daa3da
saahu27 <sahrudaypatti@gmail.com>
2022-11-05 00:07:34 -0400

    Creating and using plugins

---
+++ build/polygon_plugins/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/src/polygon_plugins
+# Build directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/build/polygon_plugins
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/polygon_plugins/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/install/polygon_base/share/polygon_base/package.sh"
