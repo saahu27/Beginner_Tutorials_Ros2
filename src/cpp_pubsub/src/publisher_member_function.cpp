83a8fa0c83d45dd75e208dbca3df0d4b327289f6
saahu27 <sahrudaypatti@gmail.com>
2022-11-05 06:05:01 -0400

    ros_pub_sub

--- src/cpp_pubsub/src/publisher_member_function.cpp
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -35,7 +35,7 @@ class MinimalPublisher : public rclcpp::Node {
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+    message.data = "Lets Try again, :) : " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
