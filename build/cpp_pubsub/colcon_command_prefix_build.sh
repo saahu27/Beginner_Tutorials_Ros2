7cda2e47a3b4081a0f1550ab2598361859dc1db3
saahu27 <sahrudaypatti@gmail.com>
2022-11-04 23:21:14 -0400

    Removing Build files

--- build/cpp_pubsub/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/install/tutorial_interfaces/share/tutorial_interfaces/package.sh"
--- build/tutorial_interfaces/tutorial_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/build/tutorial_interfaces/tutorial_interfaces__py
-# Build directory: /home/sahruday/Documents/Github/Beginner_Tutorials_Ros2/build/tutorial_interfaces/tutorial_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
