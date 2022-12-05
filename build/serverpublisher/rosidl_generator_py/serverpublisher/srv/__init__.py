e5227f13ea06af3ea4c885654616f349398d217f
saahu27 <sahrudaypatti@gmail.com>
2022-12-04 22:11:33 -0500

    ...

--- build/serverpublisher/rosidl_generator_py/serverpublisher/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from serverpublisher.srv._add_three_ints import AddThreeInts  # noqa: F401
--- build/serverpublisher/serverpublisher__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chotu/Beginner_Tutorials_Ros2/build/serverpublisher/serverpublisher__py
-# Build directory: /home/chotu/Beginner_Tutorials_Ros2/build/serverpublisher/serverpublisher__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
